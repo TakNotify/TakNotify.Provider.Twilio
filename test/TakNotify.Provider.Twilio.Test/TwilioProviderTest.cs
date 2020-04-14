561e93807dac2a68b16c720430041a02fd1c65a7
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2020-04-14 11:31:15 +0700

    Add test for failed sending message

--- test/TakNotify.Provider.Twilio.Test/TwilioProviderTest.cs
+++ test/TakNotify.Provider.Twilio.Test/TwilioProviderTest.cs
@@ -49,5 +49,32 @@ namespace TakNotify.Provider.Twilio.Test
             var endMessage = LoggerHelper.FormatLogValues(TwilioLogMessages.Sending_End, message.ToNumber, sid);
             _logger.VerifyLog(LogLevel.Debug, endMessage);
         }
+
+        [Fact]
+        public async void Send_Failed()
+        {
+            _twilioHttpClient.Setup(client => client.MakeRequestAsync(It.IsAny<TwilioHttp.Request>()))
+                .ReturnsAsync(new TwilioHttp.Response(System.Net.HttpStatusCode.BadRequest, "Error01"));
+
+            var message = new SMSMessage
+            {
+                FromNumber = "0123",
+                ToNumber = "0321",
+                Content = "Test"
+            };
+
+            var provider = new TwilioProvider(new TwilioOptions(), _twilioHttpClient.Object, _loggerFactory.Object);
+            var result = await provider.Send(message.ToParameters());
+
+            Assert.False(result.IsSuccess);
+            Assert.NotEmpty(result.Errors);
+            Assert.Contains("Error01", result.Errors[0]);
+
+            var startMessage = LoggerHelper.FormatLogValues(TwilioLogMessages.Sending_Start, message.ToNumber);
+            _logger.VerifyLog(LogLevel.Debug, startMessage);
+
+            var warningMessage = LoggerHelper.FormatLogValues(TwilioLogMessages.Sending_Failed, message.ToNumber, result.Errors);
+            _logger.VerifyLog(LogLevel.Warning, warningMessage);
+        }
     }
 }
